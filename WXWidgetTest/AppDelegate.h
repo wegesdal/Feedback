dd5639dd7e8a10772664c77d095d7783b6a67ab2
William Egesdal <william.egesdal@gmail.com>
2020-08-15 18:30:39 -1000

    Initial Commit

---
+++ WXWidgetTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WXWidgetTest
+//
+//  Created by William Egesdal on 8/15/20.
+//  Copyright © 2020 William Egesdal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
